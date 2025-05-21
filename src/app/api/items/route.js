32565f72bda9332534943b4ac0301af28fe94039
manibharathis <remasekar.96@gmail.com>
2025-05-21 15:58:02 +0530

    added req in post method

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -5,7 +5,9 @@ export  async function GET(req){
    return Response.json([{name:"nokia",model:"1234"}])
 }
 
-export  async function POST(){
+export  async function POST(req){
    // return new Response("TEST1")
+   const body = await req.json()
+   console.log(body)
    return Response.json([{message:"response message"}])
 }
