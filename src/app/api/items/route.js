bd3d2d82cc93a21cbfbf786d5b25054cac3f536a
manibharathis <remasekar.96@gmail.com>
2025-05-21 15:53:12 +0530

    added request data from get

--- src/app/api/items/route.js
+++ src/app/api/items/route.js
@@ -1,4 +1,11 @@
-export  async function GET(){
+export  async function GET(req){
    // return new Response("TEST1")
+   const type = req.nextUrl.searchParams.get('type')
+   console.log("type" , type)
    return Response.json([{name:"nokia",model:"1234"}])
+}
+
+export  async function POST(){
+   // return new Response("TEST1")
+   return Response.json([{message:"response message"}])
 }
